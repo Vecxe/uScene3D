8fed62198bbffaeaf95b973336f689e35e5baf04
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-29 21:10:25 -0300

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
   compileSdk = 33
     
   defaultConfig {
-    applicationId = "org.robok.engine"
+    applicationId = "org.robok.engine.scene"
     minSdk = 26
     targetSdk = 33
     versionCode = 1
