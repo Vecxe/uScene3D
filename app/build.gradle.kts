5f2c29d4f0cd9859fe33f5c68ad14590357abeca
Aquiles Trindade <trindadedev13@gmail.com>
2025-01-08 03:13:37 -0300

    use compose libs from androidide (oldest)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,11 +77,14 @@ dependencies {
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("androidx.fragment:fragment-ktx:1.6.1")
 
-  implementation(platform("androidx.compose:compose-bom:2024.11.00"))
+  implementation(platform("androidx.compose:compose-bom:2022.10.00"))
+  implementation("androidx.core:core-ktx:1.8.0")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+  implementation("androidx.compose.ui:ui-graphics")
+  implementation("androidx.compose.ui:ui-tooling-preview")
+  implementation("androidx.activity:activity-compose:1.5.1")
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.material3:material3")
-  implementation("androidx.compose.ui:ui-graphics")
-  implementation("androidx.activity:activity-compose:1.9.3")
 
   implementation("com.badlogicgames.gdx:gdx:1.9.14")
   implementation("com.badlogicgames.gdx:gdx-backend-android:1.9.14")
