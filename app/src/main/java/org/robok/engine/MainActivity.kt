38cf9858288adf58b1eaafe820569804b36a79b6
Aquiles Trindade <trindadedev13@gmail.com>
2025-01-08 03:21:07 -0300

    Update MainActivity.kt

--- app/src/main/java/org/robok/engine/MainActivity.kt
+++ app/src/main/java/org/robok/engine/MainActivity.kt
@@ -33,27 +33,11 @@ class MainActivity : ComponentActivity(), AndroidFragmentApplication.Callbacks {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    hideSystemUI()
     setContent {
       MaterialTheme {
         val state = rememberGDXState()
         GDXWidget(state = state)
       }
-    }
-  }
-
-  @Deprecated("Use EdgeToEdge instead.")
-  private fun hideSystemUI() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-      window.setDecorFitsSystemWindows(false)
-      window.insetsController?.let { controller ->
-        controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-        controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-      }
-    } else {
-      @Suppress("DEPRECATION")
-      window.decorView.systemUiVisibility =
-        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
     }
   }
 
