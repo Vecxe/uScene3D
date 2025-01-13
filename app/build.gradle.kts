24c88148e1a8042ad3c610358f8fe7deae90ec86
Aquiles Trindade <trindadedev13@gmail.com>
2025-01-13 02:08:14 -0300

    chore(deps): Update SDK to 24
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
   namespace = "org.robok.engine"
-  compileSdk = 34
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "org.robok.engine.scene"
     minSdk = 26
-    targetSdk = 34
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
     
