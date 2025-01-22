173fa3323606d3099875503d731b8fee021d2540
ThDev-only <rthiarley@gmail.com>
2025-01-22 19:17:24 -0300

    fix: import Spacer in MainActivity

--- app/src/main/java/org/robok/engine/MainActivity.kt
+++ app/src/main/java/org/robok/engine/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.badlogic.gdx.backends.android.AndroidFragmentApplication
@@ -39,6 +40,7 @@ import org.robok.engine.compose.rememberGDXState
 import org.robok.engine.feature.scene.editor.interfaces.EmptyObjectActionListener
 import org.robok.engine.feature.scene.editor.interfaces.ObjectListener
 import org.robok.engine.feature.scene.editor.objects.SceneObject
+
 
 class MainActivity : AppCompatActivity(), AndroidFragmentApplication.Callbacks {
 
