5c34c7ed9a3101740ae90f2c38a1b8148aed1c11
trindadedev13 <trindadedev13@gmail.com>
2025-01-24 03:11:22 -0300

    fix

--- app/src/main/java/org/robok/engine/compose/components/options/Options.kt
+++ app/src/main/java/org/robok/engine/compose/components/options/Options.kt
@@ -3,18 +3,18 @@ package org.robok.engine.compose.components.options
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.res.painterResource
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Android
 import org.robok.engine.R
 
 @Composable
-fun getOptions(): List<OptionModel> {
-  return remember {
-    listOf(
-      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 1"),
-      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 2"),
-      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 3"),
-      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 4"),
-      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 5"),
-      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 6"),
-    )
-  }
+fun rememberOptions() = remember {
+  listOf(
+    OptionModel(icon = Icons.Outlined.Android, text = "Option 1"),
+    OptionModel(icon = Icons.Outlined.Android, text = "Option 2"),
+    OptionModel(icon = Icons.Outlined.Android, text = "Option 3"),
+    OptionModel(icon = Icons.Outlined.Android, text = "Option 4"),
+    OptionModel(icon = Icons.Outlined.Android, text = "Option 5"),
+    OptionModel(icon = Icons.Outlined.Android, text = "Option 6"),
+  )
 }
