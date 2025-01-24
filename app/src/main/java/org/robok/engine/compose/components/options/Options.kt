76ab7cfef2f0d1a3b0c771a19ebfebe0e21cca41
trindadedev13 <trindadedev13@gmail.com>
2025-01-24 03:01:30 -0300

    fix: use getOptions to fix this fuck error

--- app/src/main/java/org/robok/engine/compose/components/options/Options.kt
+++ app/src/main/java/org/robok/engine/compose/components/options/Options.kt
@@ -1,16 +1,20 @@
 package org.robok.engine.compose.components.options
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.res.painterResource
 import org.robok.engine.R
 
-val options = remember {
-  listOf(
-    OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 1"),
-    OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 2"),
-    OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 3"),
-    OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 4"),
-    OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 5"),
-    OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 6"),
-  )
+@Composable
+fun getOptions(): List<OptionModel> {
+  return remember {
+    listOf(
+      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 1"),
+      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 2"),
+      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 3"),
+      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 4"),
+      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 5"),
+      OptionModel(icon = painterResource(id = R.drawable.ic_launcher_foreground), text = "Option 6"),
+    )
+  }
 }
