74db20653c0696a0818209579559e5662dc8b7a4
trindadedev13 <trindadedev13@gmail.com>
2025-01-25 16:45:37 -0300

    .

--- app/src/main/java/org/robok/engine/MainActivity.kt
+++ app/src/main/java/org/robok/engine/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
