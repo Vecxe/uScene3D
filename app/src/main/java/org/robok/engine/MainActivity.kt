f41b9a95dc3137f3b95e33f1ddc261a3834cd164
trindadedev13 <trindadedev13@gmail.com>
2025-01-26 01:46:11 -0300

    update OptionsBox

--- app/src/main/java/org/robok/engine/MainActivity.kt
+++ app/src/main/java/org/robok/engine/MainActivity.kt
@@ -121,15 +121,17 @@ class MainActivity : AppCompatActivity(), AndroidFragmentApplication.Callbacks {
         OptionsBox(modifier = Modifier.padding(10.dp))
       }
 
-      IconButton(
-        onClick = { viewModel.setOptionsOpen(!viewModel.isOptionsOpen) },
-        modifier = Modifier.size(70.dp).align(Alignment.TopEnd),
-      ) {
-        Image(
-          imageVector = Icons.Rounded.MoreVert,
-          contentDescription = stringResource(R.string.common_word_more),
-          colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
-        )
+      ExpandAndShrink(modifier = Modifier.align(Alignment.CenterEnd), visible = !viewModel.isOptionsOpen, vertically = false) {
+        IconButton(
+          onClick = { viewModel.setOptionsOpen(!viewModel.isOptionsOpen) },
+          modifier = Modifier.size(70.dp).align(Alignment.TopEnd),
+        ) {
+          Image(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = stringResource(R.string.common_word_more),
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+          )
+        }
       }
     }
   }
