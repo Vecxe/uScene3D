d0c2caccd4cd105e2d6cc15639e381eeb6af0dde
ThDev-only <rthiarley@gmail.com>
2025-02-01 18:55:32 -0300

    update code

--- app/src/main/java/org/robok/engine/MainActivity.kt
+++ app/src/main/java/org/robok/engine/MainActivity.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.badlogic.gdx.backends.android.AndroidFragmentApplication
 import com.badlogic.gdx.math.Vector2
 import org.robok.engine.compose.components.animation.ExpandAndShrink
